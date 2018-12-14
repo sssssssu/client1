c396be48ea0264f20fa33d74df58f17ed2289813
su1 <653045775@qq.com>
2018-12-14 09:54:23 +0800

    逆向工程生成表、xml文件、mapper接口

---
+++ src/main/java/com/su/ssm/client/mapper/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.su.ssm.client.mapper;
+
+import com.su.ssm.client.model.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
---
+++ src/main/java/com/su/ssm/client/model/UserInfo.java
@@ -0,0 +1,33 @@
+package com.su.ssm.client.model;
+
+public class UserInfo {
+    private String id;
+
+    private String userName;
+
+    private String userPassword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+}
